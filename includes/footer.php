d98a9fe7dd8f312c7e4d7359fec47604ccd650b3
bawantha <bawwa.16@cse.mrt.ac.lk>
2018-11-04 15:15:11 +0530

    Initial commit

---
+++ includes/footer.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Bawantha
+ * Date: 11/4/2018
+ * Time: 2:38 PM
+ */
---
+++ includes/sidebar.php
@@ -0,0 +1,12 @@
+<div class="panel panel-default sidebar-menu">
+    <div class="panel-heading">
+        <h3 class="panel-title">Product Categories</h3>
+    </div>
+    <div class="panel-body">
+        <ul class="nav nav-pills nav-stacked category-menu">
+            <li><a href="shop.php">Electronic</a></li>
+            <li><a href="shop.php">Toys</a></li>
+        </ul>
+
+    </div>
+</div>
