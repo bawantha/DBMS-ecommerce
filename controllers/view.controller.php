307b026ea60f122bfa72c74e44a53c61acb4e063
ushirask <ushira.16@cse.mrt.ac.lk>
2018-11-25 19:26:52 +0530

    view price

--- controllers/view.controller.php
+++ controllers/view.controller.php
@@ -9,4 +9,14 @@
 	foreach($detail_array as $detail){
 		$detail_value=$detail['variant_type']." : ".$detail['variant_value'];
 		echo "<li>$detail_value</li>";
-}?>
+	}
+	$sql2="SELECT calculated_price FROM sku WHERE sku_id='$sku_id'";
+	$result2=mysqli_query($db,$sql2);
+	$row2 = mysqli_fetch_array($result2,MYSQLI_ASSOC);
+	$price=$row2['calculated_price'];
+	echo"</div><div class=\"box\">
+					<h1>
+							Rs. $price 
+					</h1>
+				</div>";
+?>
